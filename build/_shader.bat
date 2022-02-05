1b1862362011e272b830369a4ab1ead159559868
Tengu712 <65766435+Tengu712@users.noreply.github.com>
2022-02-05 20:38:41 +0900

    hlsl

--- build/_shader.bat
+++ build/_shader.bat
@@ -1,3 +1,3 @@
 @echo off
-fxc /T vs_5_0 /E vs_main /Fh ../directx/_vshader.h ../directx/shader.vsh
-fxc /T ps_5_0 /E ps_main /Fh ../directx/_pshader.h ../directx/shader.psh
+fxc /T vs_5_0 /E vs_main /Fh ../directx/_vshader.h ../directx/shader_vertex.hlsl
+fxc /T ps_5_0 /E ps_main /Fh ../directx/_pshader.h ../directx/shader_pixel.hlsl
